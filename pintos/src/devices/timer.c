cbcf1cba213be2c63d8ca09b9fb350605328298d
prorata12 <prorata@naver.com>
2017-09-17 18:03:56 +0900

    make waiting_list_prior_version_real

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -103,6 +103,8 @@ timer_sleep (int64_t ticks)
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
   */
+  thread_current()->sleep_start_ticks = start;
+  thread_current()->sleep_ticks = ticks;
   thread_block();
 }
 
